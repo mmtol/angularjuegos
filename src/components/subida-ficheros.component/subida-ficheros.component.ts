7b126129aab4b6661683fac5047c2b9dcf300d90
mmtol <mmigueltolosana@gmail.com>
2025-11-13 10:45:25 +0100

    fin

--- src/components/subida-ficheros.component/subida-ficheros.component.ts
+++ src/components/subida-ficheros.component/subida-ficheros.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import ServiceFicheros from '../../services/service.ficheros';
+import Fichero from '../../models/Fichero';
 
-@Component({
-  selector: 'app-subida-ficheros.component',
+@Component
+({
+  selector: 'app-subida-ficheros',
   standalone: false,
   templateUrl: './subida-ficheros.component.html',
   styleUrl: './subida-ficheros.component.css',
 })
-export class SubidaFicherosComponent {
+export class SubidaFicherosComponent 
+{
+  public fichero!:Fichero;
+  public mensaje!:string;
 
+  @ViewChild("cajaFichero") cajaFichero:ElementRef;
+
+  constructor(private _service:ServiceFicheros)
+  {
+    this.cajaFichero = new ElementRef("");
+  }
+
+  subirFichero():void
+  {
+    let file = this.cajaFichero.nativeElement.files[0];
+    let path = this.cajaFichero.nativeElement.value.split("\\");
+    let fileName = path[2];
+    let fileContent="";
+
+    let reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () =>
+    {
+      let buffer:ArrayBuffer;
+      buffer = reader.result as ArrayBuffer;
+      var base64:string;
+      base64 = btoa(new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ''));
+      fileContent=base64;
+      this.fichero = new Fichero(fileName, fileContent);
+      this._service.subirFichero(this.fichero).then(response =>
+      {
+        console.log(response);
+        this.mensaje = "Fichero subido correctamente!!!";
+      })
+    }
+  }
 }
--- src/services/service.ficheros.ts
+++ src/services/service.ficheros.ts
@@ -1,7 +1,30 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import Fichero from "../models/Fichero";
+import { environment } from "../environments/environment.development";
 
 @Injectable()
 export default class ServiceFicheros
 {
-    
+    constructor(private _http:HttpClient){}
+
+    subirFichero(fichero:Fichero)
+    {
+        let url = environment.ficheros;
+        let endPoint = "api/TestingFiles";
+
+        let data = JSON.stringify(fichero);
+        let header = new HttpHeaders();
+        header = header.set("Content-type", "application/json");
+
+        let promise = new Promise((resolve)=>
+        {
+            this._http.post(url+endPoint, data, {headers:header}).subscribe(response =>
+            {
+                resolve(response);
+            })
+        })
+
+        return promise;
+    }
 }
